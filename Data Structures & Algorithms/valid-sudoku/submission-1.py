7d8856da443164ece81e7eb1c5e77b3ca9c9e0d9
Mohammad <149521106+mohxmmad@users.noreply.github.com>
2026-08-18 14:02:24 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,47 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        for i in range(9):
+            hashmap = set()
+            for j in range(9):
+                if board[i][j] != ".":
+                    if board[i][j] in hashmap:
+                        return False
+                    else:
+                        hashmap.add(board[i][j])
+            hashmap.clear()
+        
+        for i in range(9):
+            hashmap = set()
+            for j in range(9):
+                if board[j][i] != ".":
+                    if board[j][i] in hashmap:
+                        return False
+                    else:
+                        hashmap.add(board[j][i])
+            hashmap.clear()
+        x = 0
+        y = 0
+
+        for k in range(9):
+            hashmap = set()
+            if k > 5:
+                x = 6
+            elif k > 2:
+                x = 3
+            else:
+                x = 0
+            for i in range(x, x+3):
+                for j in range(y, y+3):
+                    if board[i][j] != ".":
+                        if board[i][j] in hashmap:
+                            return False
+                        else:
+                            hashmap.add(board[i][j])
+            hashmap.clear()
+
+            y += 3
+            y = y%9
+            
+        
+        return True
