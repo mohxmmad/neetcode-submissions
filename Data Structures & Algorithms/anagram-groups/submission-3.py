c20065561f87610c9297e0f4eb348b7631ba00b0
Mohammad <149521106+mohxmmad@users.noreply.github.com>
2026-08-18 07:11:04 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,37 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        # Frequency map list contains the frequency maps of all the visited unique strings 
+        fmaplist = {}
+        val = []
+        value_updated = False
+        counter = 0
+
+        for i, string in enumerate(strs):
+            freqmap = {}
+            freqstr = ""
+
+            if len(string) != 0:   
+                for j, char in enumerate(string):
+                    freqmap[string[j]] = 1 + freqmap.get(char, 0)
+
+                sorted_freqmap = dict(sorted(freqmap.items(), key=lambda item: item[0]))
+
+                
+                for index, (key, value) in enumerate(sorted_freqmap.items()):
+                    freqstr += key
+                    freqstr += str(value)
+            else:
+                freqstr = "NULL"
+
+            if freqstr in fmaplist:
+                index = fmaplist[freqstr]
+                val[index].append(string)
+            else:
+                fmaplist[freqstr] = counter
+                empty_list = []
+                val.append(empty_list)
+                val[counter].append(string)
+                counter += 1
+
+        return val
